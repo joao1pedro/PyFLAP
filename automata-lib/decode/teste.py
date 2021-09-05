a7c4e63ba7901e3baf60dd6dd8c5b600f5ca951f
João Pedro <jpedro@alu.ufc.br>
2021-09-05 18:02:26 -0300

    adicionando o projeto ao repositorio do gitlab

--- automata-lib/decode/teste.py
+++
@@ -1,9 +0,0 @@
-
-import decode
-
-arquivo = 'teste.jff'
-arquivoTeste = 'teste.cases'
-
-
-nfa = decode.jffToNFA(arquivo)
-print(decode.teste(nfa, arquivoTeste))
--- automata-lib/teste.py
+++
@@ -1,7 +0,0 @@
-import csv
-
-arquivoTeste = 'teste.cases'
-
-with open (arquivoTeste, 'r') as f:
-    for i in csv.reader(f, delimiter='\t'):
-        print(i)
